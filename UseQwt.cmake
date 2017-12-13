0d448b0aeb1f4651019fe8cfb8e6b1ab71dddfa7
Dan White <dwhite@sci.utah.edu>
2017-12-13 09:50:58 -0700

    Set UseFile

--- UseQwt.cmake
+++ UseQwt.cmake
@@ -1,6 +1,2 @@
-#if(NOT LodePng_FOUND)
-#  MESSAGE(FATAL_ERROR "Including UseLodePng.cmake but the LODEPNG package was not found.")
-#endif()
-
 include_directories(${SCI_QWT_INCLUDE})
 link_directories(${SCI_QWT_LIBRARY_DIR})
